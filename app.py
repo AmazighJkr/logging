b6fdb920d5d667c6ae4dba3e15bddcf7c6045170
AmazighJkr <96497816+AmazighJkr@users.noreply.github.com>
2025-02-18 10:45:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ app.secret_key = 'your_secret_key'  # Keep this secure!
 
 # MySQL Database Configuration
 app.config["MYSQL_HOST"] = "sql7.freesqldatabase.com"
-app.config["MYSQL_USER"] = "sql7762208"
-app.config["MYSQL_PASSWORD"] = "MqFJpHymhB"
-app.config["MYSQL_DB"] = "sql7762208"
+app.config["MYSQL_USER"] = "sql7763333"
+app.config["MYSQL_PASSWORD"] = "BtCcUDutUB"
+app.config["MYSQL_DB"] = "sql7763333"
 app.config["MYSQL_PORT"] = 3306
 
 mysql = MySQL(app)
